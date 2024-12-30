054ccbc93f8a7a15910ac350155b56286056d79c
Zachary Decker <deckerza@rose-hulman.edu>
2024-12-30 12:10:21 -0800

    ye

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Button, View, StyleSheet, SafeAreaView } from "react-native";
-
-import { Amplify } from "aws-amplify";
+import { Button, View, StyleSheet, SafeAreaView, Alert } from "react-native";
+import { Amplify} from "aws-amplify";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
-
 import outputs from "./amplify_outputs.json";
-
+import { deleteUser } from 'aws-amplify/auth';
 Amplify.configure(outputs);
 
 const SignOutButton = () => {
@@ -18,12 +16,38 @@ const SignOutButton = () => {
   );
 };
 
+const DeleteAccountButton = () => {
+  const handleDeleteUser = async () => {
+    try {
+      await deleteUser();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const confirmDelete = () => {
+    // console.log(Auth);
+
+    console.log("Delete button pressed"); // Debugging log
+    if (window.confirm("Are you sure you want to delete your account? This action cannot be undone.")) {
+      handleDeleteUser();
+    }
+  };
+
+  return (
+    <View style={styles.deleteAccountButton}>
+      <Button title="Delete Account" onPress={confirmDelete} color="red" />
+    </View>
+  );
+};
+
 const App = () => {
   return (
     <Authenticator.Provider>
       <Authenticator>
         <SafeAreaView>
           <SignOutButton />
+          <DeleteAccountButton />
         </SafeAreaView>
       </Authenticator>
     </Authenticator.Provider>
@@ -33,6 +57,10 @@ const App = () => {
 const styles = StyleSheet.create({
   signOutButton: {
     alignSelf: "flex-end",
+  },
+  deleteAccountButton: {
+    alignSelf: "flex-end",
+    marginTop: 10,
   },
 });
 
